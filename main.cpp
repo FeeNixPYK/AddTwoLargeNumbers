08fe1883c824b537573b79f233c3e956e102f912
FeeNixPYK <feenix.pyk@icloud.com>
2024-09-08 17:38:41 -0400

    successfully match the length difference between two strings

--- main.cpp
+++ main.cpp
@@ -1,19 +1,32 @@
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 
+
 void addzero(string &a,int b){
-    for (i=0; i < b; i++) a ='0'+a;
+    for (int i=0; i < b; i++) a ='0'+a;
 }
 
 int main() {
     //ask user to enter two numbers and store as strings.
     string num1, num2, result;
+    int length1, length2, lengthDiff, resultLength;
     cout << "Enter two numbers to add: " ;
     cin >> num1 >> num2;
 
-    //get the length of two strings and set up to use in a for loop.
-    int length1 = (num1.length()-1);
-    int length2 = (num2.length()-1);
+    //get the length of two strings and put zeros in shorter string to prepare for addition
+    length1 = num1.length();
+    length2 = num2.length();
+    lengthDiff = abs(length1-length2);
+    resultLength = (length1-1);
+
+    //made two strings equal
+    if(length1 < length2) {
+        addzero(num1,lengthDiff);
+        resultLength = (length2-1);
+    } else if(length2<length1) {
+        addzero(num2,lengthDiff);
+    }
 
 
 
