3532586930f08832a1c1f6e790511138319e75f5
FeeNixPYK <feenix.pyk@icloud.com>
2024-09-08 16:46:04 -0400

    get the length of two strings

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //ask user to enter two numbers and store as strings.
+    string num1, num2;
+    cout << "Enter two numbers to add: " ;
+    cin >> num1 >> num2;
+
+    //get the length of two strings
+    int i = num1.length();
+    int j = num2.length();
+
+    return 0;
+}
---
+++ test.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+    string myString = "Hello, World!";
+    cout << "The length of the string is: " << myString.length() << std::endl;
+
+    return 0;
+}
