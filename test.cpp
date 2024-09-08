89bf508433876ac7ba79ab8e3193c5d651c60401
Phoenix <161513433+FeeNixPYK@users.noreply.github.com>
2024-09-08 19:23:09 -0400

    Delete test.cpp

--- test.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-int main() {
-    string myString = "Hello, World!";
-    cout << "The length of the string is: " << myString.length() << std::endl;
-
-    return 0;
-}
